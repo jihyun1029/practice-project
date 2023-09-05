b6ac09b3f45e61b4aa04c1575ca71b2f84cf0f5a
jihyun1029 <parkji2028@naver.com>
2023-09-05 18:18:58 +0900

    first upload

---
+++ src/main/java/org/sp/springapp/domain/Gallery.java
@@ -0,0 +1,11 @@
+package org.sp.springapp.domain;
+
+public class Gallery {
+	
+	private int gallery_idx;
+	private String title;
+	private String writer;
+	private String regdate;
+	private int hit;
+	private String content;
+}
